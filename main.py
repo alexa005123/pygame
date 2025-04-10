f06dea886eb074db13c011e63db6e7c1f1d624da
alexa005123 <alexa005asdqwet@gmail.com>
2025-04-10 19:55:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,31 @@
 import pygame
 
-class Food():
-    def __init__(self,a,b,c):
-        self.jpg = pygame.image.load(a)
-        self.rect = self.jpg.get_rect()
-        self.x = b
-        self.y = c
+class Food:
+    def __init__(self, name_image, x, y):#конструктор, здесь создаются свойства, он вызывается при создании объекта
+        self.jpg = pygame.image.load(name_image)#загружение картинки, ЭТО СВОЙСТВО
+        self.rect = self.jpg.get_rect()#получение прямоугольника от картинки
+        self.rect.x = x
+        self.rect.y = y
 
-fon = Food()
+    def draw_image(self):# метод отрисовки кортинки
+        win.blit(self.jpg, (self.rect.x, self. rect.y))
 
-pygame.init()
-win_size = (500, 1000)
-win = pygame.display.set_mode(win_size)
-fps = pygame.time.Clock()
-C = 250
-V = 250
-background_color = (255, 255, 255)
-screen=pygame.display.set_mode(win_size)
+fon = Food('kuhnya.jpg',0,0) #создание класса food
+food1 = Food('pelmen.png', 0, 0)
+plate = Food('plate.png', 400,500)
+pygame.init()#обязательная команда
+window_size=(1000, 643)#размеры окна
+plate. size=(100,100)
+win = pygame.display.set_mode(window_size) #создание экрана с размерами window_size
+fps = pygame.time.Clock() #фпс_foo
+
+
 while True:
-    screen.blit(image,(0,0))
-
+    fon.draw_image() #применение метода отрисовки к объекту-класса food (фон)
+    food1.draw_image() #применение метода отрисовки к объекту-класса food (еда)
+    plate.draw_image()#применение метода отрисовки к объекту-класса food (тарелка)
     fps.tick(40)
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        V -= 10
-    elif keys[pygame.K_DOWN]:
-        V += 10
-    elif keys[pygame.K_RIGHT]:
-        C += 10
-    elif keys[pygame.K_LEFT]:
-        C -= 10
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.QUIT()
-    pygame.draw.circle(win, (0, 255, 255), (C, V), 52)
+    for event in pygame.event.get():#проходимся по событиям
+        if event.type == pygame.QUIT:#если нажал на крестик
+            pygame.QUIT()#выйти
     pygame.display.update()
-
-
-
-
-
-
-
